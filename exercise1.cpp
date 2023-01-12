765d86740f9cded7b2d00995191f95724fd75983
eagalis <121073612+eagalis@users.noreply.github.com>
2023-01-12 16:52:31 -0700

    *

---
+++ exercise1.cpp
@@ -0,0 +1,13 @@
+// exercise1-f23.cpp: Joshua Elder Spring 2023 eldejosh@isu.edu
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout << "Hello World from eldejosh@isu.edu\n";
+    cout << "Live long and prosper\n";
+    // test
+} 
